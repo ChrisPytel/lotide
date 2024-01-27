ad407df77bef18d835c22e94c2ae8e3e50e16674
ChrisPytel <chris_pytel@hotmail.com>
2024-01-27 12:49:10 -0500

    added first iteration of assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✌ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+//Different strings test
+assertEqual("Lighthouse Labs", "Bootcamp");
+//Identical strings test
+assertEqual(1, 1);
+//Identical strings test
+assertEqual("Canada", "Canada");
+//Different numbers test
+assertEqual(16, 32);

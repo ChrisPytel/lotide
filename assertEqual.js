f0c28c552a3ee812191eb5c5d252ba1392dcaaf2
ChrisPytel <chris_pytel@hotmail.com>
2024-01-27 18:04:43 -0500

    refactored assertEqual with string literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✌ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✌ Assertion Passed: ${actual}  === ${expected}`);
   } else {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed: ${actual}  !==  ${expected}`);
   }
 
 };

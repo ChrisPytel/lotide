fd98c5e458fe1015c327488b4f13e01258829f3f
ChrisPytel <chris_pytel@hotmail.com>
2024-01-27 19:16:13 -0500

    head.js A1 - refactored and removed comments and tests

--- head.js
+++ head.js
@@ -1,22 +1,13 @@
-
-
 const head = function(value) {
-  console.log(value[0]);
-  const firstIndexItem = value[0];
-  console.log("first item in the index is : " + typeof firstIndexItem); //checks datatype
-  return firstIndexItem;
+  return value[0];
 };
 
-
 const assertEqual = function(actual, expected) {
-  // console.log(actual);
-  // console.log(expected);
+
   if (actual === expected) {
     console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
     console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  } else {
-    console.log("something isnt correct");
   }
 };
 

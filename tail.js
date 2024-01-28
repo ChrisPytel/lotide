84f81403b861bc4f80897ec4715ea513812895e4
ChrisPytel <chris_pytel@hotmail.com>
2024-01-27 20:45:29 -0500

    tail.js A2 - added extra test cases

--- tail.js
+++ tail.js
@@ -1,8 +1,8 @@
 const tail = function(array) {
- const newArrayWithoutHead = []; 
-  for( let i = 1; i < array.length; i++){
+  const newArrayWithoutHead = [];
+  for (let i = 1; i < array.length; i++) {
     newArrayWithoutHead.push(array[i]);
-  }  
+  }
   return newArrayWithoutHead;
 };
 
@@ -16,13 +16,21 @@ const assertEqual = function(actual, expected) {
 };
 
 
-// Test Case A: Check the original array 
+// Test Case A: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+
+
 // Test Case B: Checking the returned array elements
 const result = tail(["Hello","Lighthouse","Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+assertEqual(result.length, 2);        // should pass
+assertEqual(result[0], "Lighthouse"); // should pass
+assertEqual(result[1], "Labs");       // should pass
+
+// tail of array with one element only should be empty, will pass the assert 
+assertEqual(tail(["test"]).length, 0); 
+// tail of an empty array should also be empty, will also pass the assert
+assertEqual(tail([]).length, 0); 

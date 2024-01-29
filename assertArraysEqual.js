f4385623ad1730471cf1f609132d277fb2267979
ChrisPytel <chris_pytel@hotmail.com>
2024-01-29 17:46:03 -0500

    assertArraysEqual.js A2 - Refactored so that comparisonresult is a variable assigned to eqArrays instead of passing in 3 values

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,8 @@
 //Good portion is duplicated from the other file in repo: eqArrays.js
 
 //Prints the result of eqArrays comparison algorithm.
-const assertArraysEqual = function(comparisonResult, arrA, arrB) {
+const assertArraysEqual = function(arrA, arrB) {
+  const comparisonResult = eqArrays(arrA, arrB);
   if (comparisonResult) {
     console.log(`✌ Assertion Passed: ${arrA} === ${arrB}\n`);
   } else {
@@ -11,33 +12,33 @@ const assertArraysEqual = function(comparisonResult, arrA, arrB) {
 
 
 const eqArrays = function(arr1, arr2) {
-  //sets the intial value as true. If it runs into a mismatch, sets as false and breaks the loop
-  let arrayValueMatch = true;
   //first checks if the arrays are of the same length
   if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {   
-      // logs for testing which value and datatype   
+  //sets the intial value as true. If it runs into a mismatch, sets as false and breaks the loop
+    let arrayValueMatch = true;
+    for (let i = 0; i < arr1.length; i++) {
+      // logs for testing which value and datatype are checked
       // console.log(`Comparing: ${arr1[i]} which is dataype ${typeof arr1[i]} / ${arr2[i]} which is dataype ${typeof arr2[i]}`);
       if (arr1[i] === arr2[i]) {
         console.log("🤍 Values are the same across both indeces, so far arrayValueMatch remains: " + arrayValueMatch);
       } else if (arr1[i] !== arr2[i]) {
         arrayValueMatch = false;
         console.log("💔 Value mismatch, arrayValueMatch is now: " + arrayValueMatch);
-        break; //ends the loop early
+        return false; //ends the loop early
       }
     }
-  } else{
+    return true; //if all indices are equal
+  } else {
     console.error("2 arrays passed in are not of equal length");
-    arrayValueMatch = false;
-  }
-  assertArraysEqual(arrayValueMatch, arr1, arr2);
+    return false;
+  }  
 };
 
 
 
 //Brought over the last assertions from eqArrays.js as tests
 
-// eqArrays([32, 12, 42], [32, 12, 42]); // => true
-// eqArrays([77, 76, 42], [77, 76, 46]); // => false
-// eqArrays([77, 76, 42], [77, 76]); // => ERROR, 2 arrays passed in are not of equal length
-// eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]); // => false
+// assertArraysEqual([32, 12, 42], [32, 12, 42]); // => true
+// assertArraysEqual([77, 76, 42], [77, 76, 46]); // => false
+// assertArraysEqual([77, 76, 42], [77, 76]); // => ERROR, 2 arrays passed in are not of equal length
+// assertArraysEqual([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]); // => false

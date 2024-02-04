4c15f30308baad6196c37af430750da1ff60c093
ChrisPytel <chris_pytel@hotmail.com>
2024-02-03 23:15:59 -0500

    eqObjects.js A4 - Fixed logic within eqObjects func to handle internal array comparisons more accurately

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  //first checks if the arrays are of the same length
+  if (arr1.length === arr2.length) {
+    // traditional execution with a for loop
+    for (let i = 0; i < arr1.length; i++) {
+      console.log(`Comparing: ${arr1[i]} type= ${typeof arr1[i]} / ${arr2[i]} type= ${typeof arr2[i]}`);
+      if (arr1[i] === arr2[i]) {
+        console.log("🤍 Values are the same across both indeces");
+      } else if (arr1[i] !== arr2[i]) {
+        console.log("💔 Array value mismatch");
+        return false;
+      }
+    }
+    return true;
+  } else {
+    console.error("💔 2 arrays passed in are not of the equal length");
+    return false;
+  }
+  
+};
+
+
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✌ Assertion Passed: ${actual} === ${expected}\n`);
@@ -5,6 +29,9 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}\n`);
   }
 };
+
+
+
 
 
 
@@ -28,52 +55,85 @@ const eqObjects = function(object1, object2) {
     for (const key1 in object1) {
       let keyMatch = false;
       let valueMatch = false;
-      console.log("\nBegin primary loop on first object:");
+      console.log("\nBegin primary loop on first object:\n");
       console.log("Primary key is " + key1 + " and primary value is " + object1[key1]);
 
       for (const key2 in object2) {
         console.log("Loop comparing second object");
         console.log("our key is " + key2 + " and our value is " + object2[key2]);
+
         if (key1 === key2) {
           console.log(`💛 Found matching keys: ` + key1 + " and " + key2);
           keyMatch = true;
         }
-        if (object1[key1] === object2[key2]) {
-          console.log(`🧡 Found matching values: ` + object1[key1] + " and " + object2[key2]);
-          valueMatch = true;
+
+        //if the matching values are arrays, run a check thier internal values
+        if (Array.isArray(object1[key1]) && Array.isArray(object2[key2])) {
+          console.log(`💙 Found arrays`);
+          console.log(object1[key1]);
+          console.log(object2[key2]);
+          if (eqArrays(object1[key1], object2[key2])) {
+            console.log(`💙 Found arrays with matching values: ` + object1[key1] + " and " + object2[key2]);
+            valueMatch = true;
+          } else {
+            console.log(`💢 Array content mismatch`);
+            return false;
+          }
         }
+        
+        //if it is not an array, perform the check between primitives
+        else if (Array.isArray(object1[key1]) === false) {
+          if (object1[key1] === object2[key2]) {
+            console.log(`🧡 Found matching values: ` + object1[key1] + " and " + object2[key2]);
+            valueMatch = true;
+          }
+        }
+
+
+        
       }
-      if (keyMatch === false || valueMatch === false){
+      if (keyMatch === false || valueMatch === false) {
         console.log(`💢Either a key or value was mismatched at the end of this primary loop`);
         return false; // returns false if there is mismatch between keys and values across objects
-        }
+      }
     }
   } else {
-    console.log(`objects were of different length`); 
+    console.log(`💢objects were of different length`);
     return false; // returns false if there is mismatch on # of keys
   }
-  console.log(`found no mismatches`);
+  console.log(`🙏 Found no mismatches across objects`);
   return true; //returns a true if we dont encounter no mismatch
 };
+
+
+
+
 
 
 /*``````````````````````````````````````````*/
 /*           lots of test cases             */
 /*``````````````````````````````````````````*/
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
+// const shirtObject = { color: "red", size: "medium" };
+// const anotherShirtObject = { size: "medium", color: "red" };
 // eqObjects(shirtObject , anotherShirtObject); // => true
 // assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
 
-
-const shirtMismatchA = { color: "red", size: "medium" };
-const shirtMismatchB= { size: "large", color: "red" };
+// const shirtMismatchA = { color: "red", size: "medium" };
+// const shirtMismatchB = { size: "large", color: "red" };
 // eqObjects(shirtMismatchA , shirtMismatchB); // => true
 // assertEqual(eqObjects(shirtMismatchA , shirtMismatchB), false);
 
 
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+// const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 // eqObjects(shirtObject , longSleeveShirtObject); // => false
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+// assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
+
+// const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+// eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false

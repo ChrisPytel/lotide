559b8a34760913d53ee0cc4556d144004a73a574
ChrisPytel <chris_pytel@hotmail.com>
2024-02-05 18:54:27 -0500

    letterPositions.js A3 - Refactored letterPositions() to be simpler logic, and copied in updated helper functions

--- letterPositions.js
+++ letterPositions.js
@@ -1,37 +1,27 @@
+//"letterPositions.js A3 - Refactored letterPositions() to be simpler logic, and copied in updated helper functions"
 
-//Prints the result of eqArrays comparison algorithm.
+//Prints the result of the eqArrays comparison algorithm.
 const assertArraysEqual = function(arrA, arrB) {
-  const comparisonResult = eqArrays(arrA, arrB);
-  if (comparisonResult) {
-    console.log(`✌ Assertion Passed: ${arrA} === ${arrB}\n`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}\n`);
-  }
+  if (eqArrays(arrA, arrB))  console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
+  else console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);
 };
 
-//checks the contents of two arrays to see if they match
+//eqArrays takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {  //first checks if the arrays are of the same length
-    let arrayValueMatch = true; //sets the intial value as true.
-    //If it runs into a mismatch, sets it to false breaks the loop
-    for (let i = 0; i < arr1.length; i++) {
-      // logs for testing which value and datatype are checked
-      // console.log(`Comparing: ${arr1[i]} which is dataype ${typeof arr1[i]} / ${arr2[i]} which is dataype ${typeof arr2[i]}`);
-      if (arr1[i] === arr2[i]) {
-        console.log("🤍 Values are the same across both indeces, so far arrayValueMatch remains: " + arrayValueMatch);
-      } else if (arr1[i] !== arr2[i]) {
-        arrayValueMatch = false;
-        console.log("💔 Value mismatch, arrayValueMatch is now: " + arrayValueMatch);
-        return false; //ends the loop early
-      }
-    }
-    return true; //if all indices are equal// not required actually as on ln24 it breaks if false
-  } else {
-    //console.error("2 arrays passed in are not of equal length);
+  if (arr1.length !== arr2.length) {//first checks if the arrays are of the same length
+    console.log("💥 Arrays are of different length, returning false");
     return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    // console.log("Comparing:", arr1[i], ` Datatype: ${typeof arr1[i]} with `, arr2[i], `Datatype: ${typeof arr2[i]}`);
+    if (arr1[i] !== arr2[i]) {
+      console.log("💥 Array values dont match, returning false");
+      return false;
+    }
+  }
+  console.log("🤍 Values are the same across the arrays, returning true");
+  return true;
 };
-
 
 
 // We'll implement a new function letterPositions which will return all the
@@ -41,38 +31,29 @@ const eqArrays = function(arr1, arr2) {
 //  its number of occurrences, multiple numbers may be needed to represent
 //  all the places in the string that it shows up.
 
-
 const letterPositions = function(sentence) {
   const results = {};
-  //instead of doing a for in, its easier to just create a index as its own var
   let letterIndexNumber = 0;
-
   for (const letter of sentence) {
-    if (letter === " ") {
-      // console.log("🌌 Encountered a space, skipping this");
-    } else if (results[letter]) {
-      // console.log("🚀 Exists as a letter already, adding another index #");
-      results[letter].push(letterIndexNumber);
-    } else if (letter !== " ") {
-      // console.log(letter + " is a letter at index # " + letterIndexNumber);
-      results[letter] = [letterIndexNumber];
-    }
-    // console.log("end of loop" + letterIndexNumber);
+    // if a character that isnt a space is detected, pushes the the index#
+    if (results[letter] && letter !== " ") results[letter].push(letterIndexNumber);    
+    // if a character that isnt a space is detected, logs the index#
+    else if (letter !== " ") results[letter] = [letterIndexNumber];
     letterIndexNumber++;
   }
   return results;
 };
 
+/*          Assertion tests        */
 
-
-const sequenceA = letterPositions("hello");
-console.log(sequenceA);
-assertArraysEqual(letterPositions("hello").e, [1]);
+// const sequenceA = letterPositions("hello");
+// console.log(sequenceA);
+// assertArraysEqual(letterPositions("hello").e, [1]);
 
 const sequenceB = letterPositions("lighthouse in the house");
 console.log(sequenceB);
 assertArraysEqual(letterPositions("lighthouse in the house").h, [3,5,15,18]);
 
-const sequenceC = letterPositions("Give life back to music");
-console.log(sequenceC);
-assertArraysEqual(letterPositions("Give life back to music").i, [1, 6 ,21]);
+// const sequenceC = letterPositions("Give life back to music");
+// console.log(sequenceC);
+// assertArraysEqual(letterPositions("Give life back to music").i, [1, 6 ,21]);

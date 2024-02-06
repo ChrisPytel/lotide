d2f37af80338a5d7cded68ff685f12ad335fd327
ChrisPytel <chris_pytel@hotmail.com>
2024-02-06 00:44:07 -0500

    findKey.js A1 - Function runs and returns the first key that matches our callback functions requirement

--- findKey.js
+++ findKey.js
@@ -1,4 +1,4 @@
-//"findkey.js added"
+//"findKey.js A1 - Function runs and returns the first key that matches our callback functions requirement"
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,27 +8,28 @@ const assertEqual = function(actual, expected) {
 
 
 //passes in an object and a callback, should return the first key that matches the callbacks parameter
-const findKey = function(object, callback){
+const findKey = function(object, callback) {
   console.log(`our original object`, object);
-
-  for(const key in object){
-    if (callback(key)){
-      
-      console.log(`found our match, it is: `);
-    }
-    else{
-      console.log(`no match found here`, key);
-    }    
+  for (const key in object) {
+    if (callback(object[key])) {      
+      console.log(`Found our match, it is: `, key);
+      return key;
+    } 
+    else console.log(`No match found here at: `, key);
   }
-}
+};
 
 
- findKey({
+findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
 
+/*       assertions for testing         */
+
+
+assertEqual(`potato`, `potato`);

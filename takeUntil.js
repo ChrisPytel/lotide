444f97a6402f8a630cb598b9b28f48c78dae24e9
ChrisPytel <chris_pytel@hotmail.com>
2024-02-05 23:50:41 -0500

    takeUntil.js A3 - refactored takeUntil, accidentally used a for..in instead of a for..of loop. Haha, oops :)

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,4 @@
-//"takeUntil.js A2 - takeUntil logic functions, added assertions for testing"
+//"takeUntil.js A3 - refactored takeUntil, accidentally used a for..in instead of a for..of loop. Haha, oops :)"
 
 //Prints the result of the eqArrays comparison algorithm.
 const assertArraysEqual = function(arrA, arrB) {
@@ -23,21 +23,19 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-//takeUntil takes in an array, and a callback, returns an array of elements after 
-//
-const takeUntil = function(array, callback){
+//  takeUntil function takes in an array, and a callback, returns an array of elements after
+//  checking what our callback function requires, stopping when condition is met
+const takeUntil = function(array, callback) {
   const newArray = [];
   // console.log(`our initial array is:`, array);
-    for(const element in array){
-      // console.log(`element: `, array[element]); 
-      if (callback(array[element]) === false){ 
-        newArray.push(array[element]);
-      }   
-      else break; //ends the loop as soon as the callback hits a true return
-    }
+  for (const element of array) {
+    // console.log(`element: `, array[element]);
+    if (callback(element) === false) {
+      newArray.push(element);
+    } else break; //ends the loop as soon as the callback hits a true return
+  }
   return newArray;
-}
-
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, number => number < 0);
@@ -49,7 +47,7 @@ console.log(results2);    //Expected Output [ 'I've', 'been', 'to', 'Hollywood' 
 
 
 
- /*        assertions for testing         */
+/*        assertions for testing         */
 
 assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);  // => true
 assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);  // => true

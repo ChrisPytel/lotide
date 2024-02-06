5a469fadea71b1faf5e81847d8c04ccfbcac156f
ChrisPytel <chris_pytel@hotmail.com>
2024-02-06 00:09:54 -0500

    findKeyByValue.js A4 - Refactored linted and removed console.logs

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,48 +1,41 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✌ Assertion Passed: ${actual} === ${expected}\n`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}\n`);
-  }
-};
+//findKeyByValue.js A4 - "Refactored linted and removed console.logs"
 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
+};
 
 /*    Implement the function findKeyByValue which takes in an object and a value.
 It should scan the object and return the first key which contains the given value.
 If no key with that given value is found, then it should return undefined.    */
 
 const findKeyByValue = function(object, value) {
-    
+  //console.log(`We are searching for ${value}\n`);
   for (const key in object) {
-    console.log(key);
-    console.log(object[key]);
-
+    //console.log(`Our key is:`,key ,`and our value is: `, object[key]);
     if (value === object[key]) {
-      console.log("📺 WOW! We got a match! 🍿");
-      console.log(object[key] + " Matches with " + value);
-      //confusing, but returns the key from the object, aka the genre in this case
-      return key;
+      console.log("📺 WOW! We got a match! " + object[key] + " Matches with " + value);
+      return key; //confusing, but returns the key from the object, aka the genre in this case
     }
   }
 };
 
-
-
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  action: "Sons of Anarchy",
+  drama:  "The Sopranos",
   mystery: "Stranger Things",
-  animated: "Avatar: The Last Airbender",
+  action: "Sons of Anarchy",
+  workplaceComedy: "The Office",
   fantasy: "Game of Thrones",
   political: "House of Cards",
   foreign: "Squid Game",
-  workplace_comedy: "The Office"
+  animated: "Avatar: The Last Airbender"
 };
 
+/*            assertions for testing             */
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // =>  pass
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Sopranos"), "drama"); // =>  pass
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // =>  pass
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Game of Thrones"), "fantasy"); // =>  pass
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Attack on Titan"), "anime"); // =>  fail

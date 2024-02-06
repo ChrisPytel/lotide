0e2fb740644299014b29bf81fb11feb1a88eca5e
ChrisPytel <chris_pytel@hotmail.com>
2024-02-06 00:58:01 -0500

    findKey.js A2 - Added assertions to check findKey versatility

--- findKey.js
+++ findKey.js
@@ -1,15 +1,13 @@
-//"findKey.js A1 - Function runs and returns the first key that matches our callback functions requirement"
+//"findKey.js A2 - Added assertions to check findKey versatility"
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
 };
 
-
-
 //passes in an object and a callback, should return the first key that matches the callbacks parameter
 const findKey = function(object, callback) {
-  console.log(`our original object`, object);
+  console.log(`\nOur original object is: `, object);
   for (const key in object) {
     if (callback(object[key])) {      
       console.log(`Found our match, it is: `, key);
@@ -19,8 +17,7 @@ const findKey = function(object, callback) {
   }
 };
 
-
-findKey({
+const skiHills = findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
@@ -29,7 +26,18 @@ findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2); // => "noma"
 
+const pizzaPlaces = findKey({
+  "Papa Johns":   { stars: 4 },
+  "Pizza Hut":    { stars: 2 },
+  "Dominos":      { stars: 3 },
+  "Pizza Pizza":  { stars: 2 },  
+  "Otherside":    { stars: 5 },
+  "241":          { stars: 1 },
+  "Grazie Ristorante":      { stars: 5 }
+}, x => x.stars === 5); // => "Otherside"
+
+
 /*       assertions for testing         */
 
-
-assertEqual(`potato`, `potato`);
+assertEqual(skiHills, "noma");// => True
+assertEqual(pizzaPlaces, "Otherside");// => True

f76b5823dfbabc3af463cc2b3cc99b289a180904
ChrisPytel <chris_pytel@hotmail.com>
2024-02-05 22:18:31 -0500

    map.js added

---
+++ map.js
@@ -0,0 +1,23 @@
+//our initial array for the map function
+const words = ["How", "I", "wish", "you", "were", "here"];
+
+
+
+
+// const test1 = words.map(word => word[0]);
+// console.log(test1);
+
+
+
+
+const map = function(array, callback){
+  // temporary code:
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
+  const results = [];
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);

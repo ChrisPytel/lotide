325542745a9c15db1910cc41a677f02bb4274872
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:32:44 -0500

    assertEqualTest.js A1 - Added comments and a type coercion test case

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,9 +1,11 @@
-//assertEqualTest.js created - Used to test assertEqual.js
+//"assertEqualTest.js A1 - Added comments and a type coercion test case"
 
-const assertEqual = require('../assertEqual');
+const assertEqual = require('../assertEqual'); //imports the function from our assertEqual.js module 
 
 //Test runner code
-assertEqual("Labrador", "Golden Sheppard");   //Different strings test
-assertEqual(1, 1);                            //Identical strings test
-assertEqual("Canada", "Canada");              //Identical strings test
-assertEqual(16, 32);                          //Different numbers test
+assertEqual("Labrador", "Golden Sheppard");   //Different strings test | Assert will FAIL
+assertEqual(1, 1);                            //Identical strings test | Assert will PASS
+assertEqual("Canada", "Canada");              //Identical strings test | Assert will PASS
+assertEqual(16, 32);                          //Different numbers test | Assert will FAIL
+
+assertEqual(12, "12");                        //String -> Integer type coercion test | Assert will FAIL

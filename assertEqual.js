fbe9b1c27a10bf538239a0e9dd42f9e90c31f012
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 13:04:40 -0500

    assertEqual.js A5 - Separated test driver code and placed into test/assertEqualTest.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,8 @@
-//"assertEqual.js A4 - Added module.export, commented out previous test driver code"
+//"assertEqual.js A5 - Separated test driver code and placed into test/assertEqualTest.js"
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
 };
-
-// assertEqual("Lighthouse Labs", "Bootcamp");   //Different strings test
-// assertEqual(1, 1);                            //Identical strings test
-// assertEqual("Canada", "Canada");              //Identical strings test
-// assertEqual(16, 32);                          //Different numbers test
 
 module.exports = assertEqual;

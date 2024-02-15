d2034b2fdcd6eab2be959b391260979a8418a97e
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 00:04:11 -0500

    assertEqual.js A3 - Added module.export

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,13 @@
-//FUNCTION IMPLEMENTATION
+//"assertEqual.js A3 - Added module.export"
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
 };
 
-//Different strings test
-assertEqual("Lighthouse Labs", "Bootcamp");
-//Identical strings test
-assertEqual(1, 1);
-//Identical strings test
-assertEqual("Canada", "Canada");
-//Different numbers test
-assertEqual(16, 32);
+assertEqual("Lighthouse Labs", "Bootcamp");   //Different strings test
+assertEqual(1, 1);                            //Identical strings test
+assertEqual("Canada", "Canada");              //Identical strings test
+assertEqual(16, 32);                          //Different numbers test
+
+module.exports = assertEqual;

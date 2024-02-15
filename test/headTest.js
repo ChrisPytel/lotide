99a11cbff0a13a90c9b78196cdf51c59608334a8
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:37:42 -0500

    headtest.js A2 - updated comments

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,11 @@
-//"headtest.js A1 - added line 4- const head = require('../head')"
+//"headtest.js A2 - updated comments"
 
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 //Test runner code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([5,6,7]), 5);  
+//5 is the first item in the array - Assert PASS   
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+//"Hello is the first item in the array - Assert PASS

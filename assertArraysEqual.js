09ac450d7cc9e0c95dbfadca4ee74da0820f2bb6
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:39:27 -0500

    assertArraysEqual.js A4 - Removed eqArrays function from this file and instead imported eqArrays.js module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,13 @@
-//"assertArraysEqual.js A3 - Refactored so that comparisonresult variable within assertArraysEqual is skipped entirely"
+//"assertArraysEqual.js A4 - Removed eqArrays function from this file and instead imported eqArrays.js module"
 
-//Prints the result of the eqArrays comparison algorithm.
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arrA, arrB) {
   if (eqArrays(arrA, arrB))  console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
    else console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);  
 };
 
-
-//eqArrays takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {//first checks if the arrays are of the same length
-    console.log("💥 Arrays are of different length, returning false");
-    return false;  
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    // console.log("Comparing:", arr1[i], ` Datatype: ${typeof arr1[i]} with `, arr2[i], `Datatype: ${typeof arr2[i]}`);
-    if (arr1[i] !== arr2[i]) {
-      console.log("💥 Array values dont match, returning false");
-      return false;
-    }
-  }  
-  console.log("🤍 Values are the same across the arrays, returning true"); 
-  return true;
-};
+module.exports = assertArraysEqual;
 
 
 /*        Brought over the last assertions from eqArrays.js as tests        */

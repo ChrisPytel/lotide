220115ede7733121ce04ff872a108cf1c0b10294
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:47:45 -0500

    assertArraysEqual.js A6 - comments updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,17 @@
-//"assertArraysEqual.js A6 - small linting fix"
+//"assertArraysEqual.js A6 - comments updated"
 
-const eqArrays = require('./eqArrays');
+const eqArrays = require('./eqArrays'); 
+
+//assertArraysEqual asserts two arrays against each other utilizing the imported eqArrays module
+//Console.logs the result of the assertions if they pass or fail
 
 const assertArraysEqual = function(arrA, arrB) {
-  if (eqArrays(arrA, arrB)) console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
-  else console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);
+  if (eqArrays(arrA, arrB)){
+    console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
+  }
+  else {
+    console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);
+  }  
 };
 
-module.exports = assertArraysEqual;
+module.exports = assertArraysEqual; //exports our assertArraysEqual function to communicate across modules

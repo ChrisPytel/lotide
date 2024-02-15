719a08a5d567010365f9f28fcfd081fbd21a3748
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:21:13 -0500

    eqArraysTest.js A2 - small test runner code changes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,9 @@
-//"eqArraysTest.js A1 - Refactored and removed unnessecary tests"
+//"eqArraysTest.js A2 - updated some test runner code"
 
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 //Test Runner code
 assertEqual(eqArrays([16, 32, 64], [16, 32, 64]), true); // => should PASS Assertion check
-assertEqual(eqArrays([77, 76, 42], [77, 76]), true); // =>different length arrays, should FAIL Assertion check
+assertEqual(eqArrays([77, 76, 42], [77, 76]), false); // => different length arrays, should PASS Assertion check
 assertEqual(eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]), false); // => should PASS assertion check
-assertEqual(eqArrays("tomato", "tomAto"), true); // => should FAIL (also strings work here for some reason when checking arrays. Neat!)

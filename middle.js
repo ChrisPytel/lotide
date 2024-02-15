432afc05e3106c50153ba04af1bccee5ba4c9435
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:51:41 -0500

    middle.js A5 - fixed comments error

--- middle.js
+++ middle.js
@@ -1,12 +1,13 @@
-//"middle.js A5 - Refactored away some console.logs, added comments"
+//"middle.js A5 - fixed comments error"
 
-// Middle takes in an array as an parameter and will returns the middle value of the array if its length is odd.
+// Middle takes in an array and returns an new array containing the middle value or values
+// Returns the 1 value from the middle of the array if the length is odd
 // Returns the 2 values from the middle of the array if the length is even
 // Returns an empty array if its length is under 3 total indexes as there is no middle
 
 const middle = function(arr) {
   const arrayMid = [];
-  if (arr.length < 3) { //returns empty array if arr.l
+  if (arr.length < 3) {
     return arrayMid; 
   } else if (arr.length % 2 === 0) { //checks with modulus operator if our array is even
     arrayMid.push(arr[(arr.length - 2) / 2]);

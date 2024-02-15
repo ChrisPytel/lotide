12db69da47742a8032b185af3054139c7946ca0b
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 16:31:18 -0500

    middleTest.js A2 - Added some additional comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,22 @@
-//"middleTest.js A1 - Refactored testing logic to utilize the modules"
+//"middleTest.js A2 - Added some comments"
 
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 //Test runner code
-// middle([1, 2]) // => returns empty array
-// middle([1, 2, 3, 4, 5]) // => [3]
-// middle(1, 2, 3, 4, 5, 6, 7, 8) // => [4, 5]
 
-//for longer arrays
-const longArray1 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-const longArray2 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
-assertArraysEqual(middle(longArray1), [18]); // => Assert Passed!
-assertArraysEqual(middle(longArray2), [39, 40]); // => Assert Passed!
+//for initially testing the middle function on its own
+// middle([1, 2]) // returns empty array
+// middle([1, 2, 3, 4, 5]) // returns [3]
+// middle(1, 2, 3, 4, 5, 6, 7, 8) // returns [4, 5]
 
-//generates a customized SUPERLONG array of numbers between our intialized contitions
+//for testing longer arrays
+const longArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; //array length is odd, should return [8]
+const longArray2 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]; // array length is even, should return [36, 37]
+assertArraysEqual(middle(longArray1), [8]); // => Assert Passed!
+assertArraysEqual(middle(longArray2), [36, 37]); // => Assert Passed!
+
+//generates a customized SUPERLONG array of numbers from our intialized for loop contitions
 const reallyLongArray = [];
-for (let i = 160; i <= 201; i++) reallyLongArray.push(i);
+for (let i = 160; i <= 201; i++) reallyLongArray.push(i); //middle of this should be 2 numbers [180, 181]
 assertArraysEqual(middle(reallyLongArray), [180, 181]);// => Assert Passed!

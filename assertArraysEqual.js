fda9b72fbcee2980727c578df8a9772e4be8b14d
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:48:23 -0500

    assertArraysEqual.js A6 - small linting fix

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
-//"assertArraysEqual.js A5 - Added module.exports and moved test runner code into test/assertArraysEqualTest.js"
+//"assertArraysEqual.js A6 - small linting fix"
 
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrA, arrB) {
-  if (eqArrays(arrA, arrB))  console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
-   else console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);  
+  if (eqArrays(arrA, arrB)) console.log(`✅ Assertion Passed: ${arrA} === ${arrB}`);
+  else console.log(`🛑 Assertion Failed: ${arrA} !==  ${arrB}`);
 };
 
 module.exports = assertArraysEqual;

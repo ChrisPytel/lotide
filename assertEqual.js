241aacf6bda9e259ca947232281d769f5b7363b4
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:24:11 -0500

    assertEqual.js A6 - Readded curlybraces for readbility and added comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,15 @@
-//"assertEqual.js A5 - Separated test driver code and placed into test/assertEqualTest.js"
+//"assertEqual.js A5 - Readded curlybraces for readbility and added comments"
+
+//AssertEqual checks two values against each other (strictly without coercion)
+//Console.logs the result if they match or not
 
 const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  if (actual === expected){
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }   
+  else{
+    console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }   
 };
 
-module.exports = assertEqual;
+module.exports = assertEqual; //exports our assertEqual function to communicate across modules

fcc8fc85cfa8ad24cd8eb75ef69c1fa1047d8530
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:44:57 -0500

    assertArraysEqual.js A5 - Added module.exports and moved test runner code into test/assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-//"assertArraysEqual.js A4 - Removed eqArrays function from this file and instead imported eqArrays.js module"
+//"assertArraysEqual.js A5 - Added module.exports and moved test runner code into test/assertArraysEqualTest.js"
 
 const eqArrays = require('./eqArrays');
 
@@ -8,11 +8,3 @@ const assertArraysEqual = function(arrA, arrB) {
 };
 
 module.exports = assertArraysEqual;
-
-
-/*        Brought over the last assertions from eqArrays.js as tests        */
-
-assertArraysEqual([32, 12, 42], [32, 12, 42]); // => true
-assertArraysEqual([77, 76, 42], [77, 76, 46]); // => false
-assertArraysEqual([77, 76, 42], [77, 76]); // => ERROR, 2 arrays passed in are not of equal length
-assertArraysEqual([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]); // => false

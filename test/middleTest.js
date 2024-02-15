55fb9e45f16769069b7f91145d28f0e22e5dc16f
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 15:52:17 -0500

    middleTest.js A1 - Refactored testing logic to utilize the modules

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,25 +1,20 @@
-//"middleTest.js created - used to test middle.js "
+//"middleTest.js A1 - Refactored testing logic to utilize the modules"
 
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
-
-const eqArrays = middle('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
 
 //Test runner code
-// middle([1, 2]) // => []
-// middle([1, 2, 3]) // => [2]
+// middle([1, 2]) // => returns empty array
+// middle([1, 2, 3, 4, 5]) // => [3]
+// middle(1, 2, 3, 4, 5, 6, 7, 8) // => [4, 5]
 
+//for longer arrays
 const longArray1 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-middle(longArray1) // => [18] is the middle with 15 indices
 const longArray2 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
-middle(longArray2); // => [39, 40] are the middle with 18 indices
+assertArraysEqual(middle(longArray1), [18]); // => Assert Passed!
+assertArraysEqual(middle(longArray2), [39, 40]); // => Assert Passed!
 
-//for customized super longtests
-  const reallyLongArray = [];
-    for (i = 162; i < 194; i++){ //creates an array of numbers between our intialized contitions
-    reallyLongArray.push(i);
-  }
-
-// middle(reallyLongArray);
-
-assertArraysEqual(eqArrays(reallyLongArray, []), true );
+//generates a customized SUPERLONG array of numbers between our intialized contitions
+const reallyLongArray = [];
+for (let i = 160; i <= 201; i++) reallyLongArray.push(i);
+assertArraysEqual(middle(reallyLongArray), [180, 181]);// => Assert Passed!

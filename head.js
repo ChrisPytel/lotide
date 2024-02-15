a463914352cf8d522c9ce8a1e734ba6f2118c16c
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:35:14 -0500

    head.js A5 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head;

--- head.js
+++ head.js
@@ -1,5 +1,6 @@
-//"head.js A3 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head"
+//"head.js A5 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head"
 
+//Head takes in an array as a parameter, and returns the first value found in it.
 const head = function(array) {
   return array[0];
 };

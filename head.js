d352fef50fc67f0f4943712830abb1f12cb1c995
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 13:06:11 -0500

    head.js A3 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head

--- head.js
+++ head.js
@@ -1,22 +1,7 @@
+//"head.js A3 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head"
+
 const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-
-
-
-
-
-
+module.exports = head;

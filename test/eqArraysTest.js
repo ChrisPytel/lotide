8544db8d51871d9113b739600eae2aaae0290d89
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:11:52 -0500

    eqArraysTest.js A1 - Refactored and removed unnessecary tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,17 +1,10 @@
-//"eqArraysTest.js created - used to test eqArrays.js"
+//"eqArraysTest.js A1 - Refactored and removed unnessecary tests"
 
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 //Test Runner code
-
-//for testing eqArrays on its own
-eqArrays([32, 12, 42], [32, 12, 42]); // => true
-eqArrays([77, 76, 42], [77, 76, 46]); // => false
-eqArrays([77, 76, 42], [77, 76]); // => false, different length arrays
-eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]); // => false
-
-//for testing eqArrays in conjunction with assertEqual
-assertEqual("tomato", "tomAto"); // => should FAIL (also strings work here for some reason when checking arrays. Neat!)
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS  Assertion check
-assertEqual(eqArrays([16, 32, 64], [16, 32, 128]), true); // => should FAIL Assertion check
+assertEqual(eqArrays([16, 32, 64], [16, 32, 64]), true); // => should PASS Assertion check
+assertEqual(eqArrays([77, 76, 42], [77, 76]), true); // =>different length arrays, should FAIL Assertion check
+assertEqual(eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]), false); // => should PASS assertion check
+assertEqual(eqArrays("tomato", "tomAto"), true); // => should FAIL (also strings work here for some reason when checking arrays. Neat!)

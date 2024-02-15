e3eba73bd32747eb0870cce4f0c997fac9765c36
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 14:24:53 -0500

    eqArrays.js A6 - refactored some lines to be inline

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,5 @@
-//"eqArrays.js A5 - removed console logs completely, and refactored some lines to be inline"
+//"eqArrays.js A6 - refactored some lines to be inline, and removed console.logs completely"
 
-//eqArrays takes in two arrays and returns true or false, based on a perfect match across indexes
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {

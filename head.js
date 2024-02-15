91c5bb4831bf28326db7272efbabdc59df0c76d1
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 17:39:37 -0500

    head.js A6 - Added comments

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
-//"head.js A5 - Refactored test runner code into test/headTest.js, and added line 7 - module.exports = head"
+//"head.js A6 - Added comments"
 
 //Head takes in an array as a parameter, and returns the first value found in it.
 const head = function(array) {
   return array[0];
 };
 
-module.exports = head;
+module.exports = head; //exports our assertEqual function to communicate across modules

0eabdba0d14b7f2b2ed22ae9d53e5b0d002d51d1
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 16:45:59 -0500

    eqArraysTest.js A3 - Reformatted assertions to use mocha n chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,22 @@
-//"eqArraysTest.js A2 - updated some test runner code"
+//"eqArraysTest.js A3 - Reformatted assertions to use mocha n chai"
 
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual'); // now using mocha for assertion tests
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-//Test Runner code
-assertEqual(eqArrays([16, 32, 64], [16, 32, 64]), true); // => should PASS Assertion check
-assertEqual(eqArrays([77, 76, 42], [77, 76]), false); // => different length arrays, should PASS Assertion check
-assertEqual(eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22]), false); // => should PASS assertion check
+
+//Mocha assertion Test runner code
+describe("#eqArrays", () => {
+
+  it("should return true for if arrays match", () => {
+    assert.isTrue(eqArrays([16, 32, 64, 128], [16, 32, 64, 128])); 
+  }); 
+
+  it("should return false for arrays with a mismatch", () => {
+    assert.isFalse(eqArrays([77, 76, 42, 66, 34, 22], [77, 76, 42, 63, 34, 22])); 
+  }); 
+
+  it("should return false for arrays of different length", () => {
+    assert.isFalse(eqArrays([77, 76, 42], [77, 76])); 
+  }); 
+});

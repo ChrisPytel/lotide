a71727e0177752796837b9f38b8a39daf42df9af
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 17:01:37 -0500

    findKeyByValue.js A5 - Refactored our test cases into test/findKeyByValueTest.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,41 +1,15 @@
-//findKeyByValue.js A4 - "Refactored linted and removed console.logs"
+//"findKeyByValue.js A5 - Refactored our test cases into test/findKeyByValueTest.js"
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
-};
-
-/*    Implement the function findKeyByValue which takes in an object and a value.
-It should scan the object and return the first key which contains the given value.
-If no key with that given value is found, then it should return undefined.    */
+//findKeyByValue takes in an object and a value.
+//It iterates through the object and return the first key which contains the given value.
+//If no key with that value is found, then it should return undefined.
 
 const findKeyByValue = function(object, value) {
-  //console.log(`We are searching for ${value}\n`);
   for (const key in object) {
-    //console.log(`Our key is:`,key ,`and our value is: `, object[key]);
-    if (value === object[key]) {
-      console.log("📺 WOW! We got a match! " + object[key] + " Matches with " + value);
-      return key; //confusing, but returns the key from the object, aka the genre in this case
+    if (value === object[key]) { //if a match is detected returns the key
+      return key;
     }
   }
 };
 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Sopranos",
-  mystery: "Stranger Things",
-  action: "Sons of Anarchy",
-  workplaceComedy: "The Office",
-  fantasy: "Game of Thrones",
-  political: "House of Cards",
-  foreign: "Squid Game",
-  animated: "Avatar: The Last Airbender"
-};
-
-/*            assertions for testing             */
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Sopranos"), "drama"); // =>  pass
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // =>  pass
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Game of Thrones"), "fantasy"); // =>  pass
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Attack on Titan"), "anime"); // =>  fail
+module.exports = findKeyByValue;  //exports our findKeyByValue function to communicate across modules

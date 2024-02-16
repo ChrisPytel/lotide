f1da0e3951e3513df715846c6a792425e0571cdf
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 19:23:22 -0500

    countOnly.js A3 - Refactored console.logs out and moved tests to test/countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -1,61 +1,23 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-
-};
+//"countOnly.js A3 - Refactored console.logs out and moved tests to test/countOnly.js"
 
 
+// countOnly takes in two parameters
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+// it will create a new object and log how many times that item appears, then returns results
+
 const countOnly = function(allItems, itemsToCount) {
-  const results  = {};
-
+  const results = {};
   for (const item of allItems) {
-
-    console.log("\nFirst loop, our guest is");
-    console.log(item);
-     
-
-    if (itemsToCount[item]) { //checks the vip list, if any returns true, this resolves and continues
-      console.log(`Is this guest a VIP?`);
-      console.log(itemsToCount[item]);
-      if (results[item]) { //if member already was pushed into the new obj, add +1 to count
+    if (itemsToCount[item]) { //itemsToCount list, if any returns true, this resolves and continues
+      if (results[item]) { //if item already was pushed into the new results object, add +1 to count
         results[item] += 1;
-      } else { //if member only appears once, list there only being 1 guest of that name
+      } else { //if item only appears once in allItems, list there only being 1 item of that name
         results[item] = 1;
       }
-    }
-    
+    }    
   }
-  console.log("\nOur final list is:");
-  console.log(results);
   return results;
 };
 
-//list of all guests
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-
-//vips to check against and thier status
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-//checks
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly; //exports our countOnly function to communicate across modules

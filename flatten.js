a0b5ffb9e77b26c24147155367cdb4c8afef332d
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 19:02:50 -0500

    flatten.js A2 - refactored logic within flatten to deal with infinitely nested arrays as well

--- flatten.js
+++ flatten.js
@@ -1,20 +1,26 @@
-//"flatten.js A1 - Refactored flatten logic, and exports the module. Moved test cases into test/flattenTest.js"
+//"flatten.js A2 - refactored logic within flatten to deal with infinitely nested arrays as well"
 
-//flatten will take in an array as an argument (possibly containing nested arrays)
+//flatten will take in an array as an argument (potentially containing many nested arrays)
 //and return a single array with all of the values "flattened".
+//Now contains logic for dealing with infinitely nested arrays via recursion and a new helper function within flatten
 
 const flatten = function(arr) {
   const flattendArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {  //checks if the value is an array      
-      for (let i2 = 0; i2 < arr[i].length; i2++) { //goes thru subarrays and pushes them 
-        flattendArray.push(arr[i][i2]);
+  //defines a function so we can recursively 
+  function nestedArrayFlattener(subArr) {
+    for (let i = 0; i < subArr.length; i++) {
+      if (Array.isArray(subArr[i])) { // if a subarray is detected recursively call nestedArrayFlattener
+        nestedArrayFlattener(subArr[i]); // and pass in the nested arrays we are on in the for loop cycle
+      } else {// if its a non-array elements push to flattendArray
+        flattendArray.push(subArr[i]); 
       }
-    } else { //if its not a nested subarray, push the value into our flattenArray as normal
-      flattendArray.push(arr[i]); 
     }
-  }
+  }  
+  //call our nestedArrayFlattener helper and passes the initial array in
+  nestedArrayFlattener(arr); 
   return flattendArray;
 };
+
+
 
 module.exports = flatten; //exports our flatten function to communicate across modules

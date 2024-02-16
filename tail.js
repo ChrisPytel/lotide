372211f3bbdcf01e91322962d92ccbfd39ba483f
ChrisPytel <chris_pytel@hotmail.com>
2024-02-15 20:56:52 -0500

    tail.js A2 - added comments

--- tail.js
+++ tail.js
@@ -1,11 +1,15 @@
-//"tail.js A1 - Refactored test cases into test/tailTest.js, and added line 11 - module.export = tail"
+//"tail.js A2 - added comments"
+
+//tail takes in an array, and returns the result of everything that isnt the first element
+//index starts at 1, effectively ignoring index 0 of our array
+//then it just pushes everything to our newArrayWithoutHead and returns our new array after loop completes
 
 const tail = function(array) {
   const newArrayWithoutHead = [];
-  for (let i = 1; i < array.length; i++) {
-    newArrayWithoutHead.push(array[i]);
+  for (let i = 1; i < array.length; i++) {  
+    newArrayWithoutHead.push(array[i]); 
   }
   return newArrayWithoutHead;
 };
 
-module.exports = tail;
+module.exports = tail;  //exports our tail function to communicate across modules

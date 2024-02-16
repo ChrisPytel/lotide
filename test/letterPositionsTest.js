f8a1da06610d5dc0b0b0fbcd0d583375efabc58f
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 18:43:08 -0500

    letterPositionsTest.js A1 - refactored test assertions

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,8 +1,6 @@
-//"letterPositionsTest.js added - for testing letterPostions.js"
+//"letterPositionsTest.js A1 - refactored test assertions"
 
 const assert = require('chai').assert;
-// const eqArrays = require('../eqArrays');     //using mocha n chai instead
-// const assertArraysEqual = require('../assertArraysEqual'); 
 const letterPositions = require('../letterPositions');
 
 
@@ -10,32 +8,28 @@ const letterPositions = require('../letterPositions');
 /*          Assertion tests        */
 
 const sequenceA = letterPositions("hello");
-// console.log(sequenceA);
-// assertArraysEqual(letterPositions("hello").e, [1]);
+// sequenceA.e should be [1]);
 
 const sequenceB = letterPositions("lighthouse in the house");
-// console.log(sequenceB);
-// assertArraysEqual(letterPositions("lighthouse in the house").h, [3,5,15,18]);
+// sequenceB.h should be [3,5,15,18]);
 
-const sequenceC = letterPositions("Give life back to music");
-// console.log(sequenceC);
-// assertArraysEqual(letterPositions("Give life back to music").i, [1, 6 ,21]);
+const sequenceC = letterPositions("I'm listening to chill lofi right now");
+// sequenceB.x should be undefined as it doesnt appear in the string ^
 
 
 //Mocha assertion Test runner code
 describe("#letterPositions", () => {
 
-  it("should return [3,5,15,18] for letter `h` in `lighthouse in the house`", () => {
-    assert.deepEqual(sequenceB.h, [3,5,15,18])
-  }); 
-  
-  it("should return [3,5,15,18] for letter `h` in `lighthouse in the house`", () => {
-    assert.deepEqual(letterPositions("lighthouse in the house").h, [3,5,15,18])
-  }); 
+  it("should return [1] for letter `e` in `hello`", () => {
+    assert.deepEqual(sequenceA.e, [1]);
+  });
 
   it("should return [3,5,15,18] for letter `h` in `lighthouse in the house`", () => {
-    assert.deepEqual(letterPositions("lighthouse in the house").h, [3,5,15,18])
-  }); 
+    assert.deepEqual(sequenceB.h, [3, 5, 15, 18]);
+  });
 
+  it("should return undefined for letter `x` in `I'm listening to chill lofi right now`", () => {
+    assert.isUndefined(sequenceC.x);
+  });
 
 });

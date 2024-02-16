684d7050d3595e2ae431c4a5d5e291541d9e7ce3
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 15:53:49 -0500

    assertObjectsEqualTest.js added - used for testing assertObjectsEqual.js

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,14 @@
+//"assertObjectsEqualTest.js - used for testing assertObjectsEqual.js"
+
+
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+//Test runner code
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject); // => should PASS assertion
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject); // => should PASS assertion

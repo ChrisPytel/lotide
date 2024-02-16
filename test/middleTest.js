c832bcdc1357d9d05d0041a375242c55a445506a
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 00:01:10 -0500

    middleTest.js A3 - added more edge tests for mocha n chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,5 @@
-//"middleTest.js A2 - refactored test cases to use mocha n chai, commented out old tests"
+//"middleTest.js A3 - added more edge tests for mocha n chai"
 
-// const assertArraysEqual = require('../assertArraysEqual');  //using mocha instead
 const assert = require('chai').assert;
 const middle = require('../middle');
 
@@ -9,17 +8,28 @@ const longArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; //array 
 const longArray2 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]; // array length is even, should return [36, 37]
 
 //generates a customized SUPERLONG array of numbers from our intialized for loop contitions
-// const reallyLongArray = [];
-// for (let i = 160; i <= 201; i++) reallyLongArray.push(i);  //middle of this should be 2 numbers [180, 181]
-// assertArraysEqual(middle(reallyLongArray), [180, 181]);// => Assert Passed!
+  // const assertArraysEqual = require('../assertArraysEqual');  //using mocha instead
+  // const reallyLongArray = [];
+  // for (let i = 160; i <= 201; i++) reallyLongArray.push(i);  //middle of this should be 2 numbers [180, 181]
+  // assertArraysEqual(middle(reallyLongArray), [180, 181]);// => Assert Passed!
 
 //Mocha assertion Test runner code
 describe("#middle", () => {
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+  
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []); 
+  });
+
   it("returns [8] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]", () => {
     assert.deepEqual(middle(longArray1), [8]); 
   });
 
   it("returns [36, 37] for [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]", () => {
     assert.deepEqual(middle(longArray2), [36, 37]); 
+     
   });
 });

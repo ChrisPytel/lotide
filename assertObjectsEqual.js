735d13fb1113c0337027ec0cd1e4060c1b7f714a
ChrisPytel <chris_pytel@hotmail.com>
2024-02-17 00:22:08 -0500

    assertObjectsEqual.js A6 - updated function description comment

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,10 +1,9 @@
-//"assertObjectsEqual.js A4 - imported eqObjects module and refactored test into test/assertObjectsEqualTest.js"
-
+//"assertObjectsEqual.js A5 - imported eqObjects module and refactored test into test/assertObjectsEqualTest.js"
 
 const eqObjects = require('./eqObjects'); //imports our eqObjects.js module for this file
 
-
-// assertObjectsEqual takes in two objects and console.logs if theyre equal or different
+//assertObjectsEqual takes in two objects and console.logs if theyre equal or different,
+//passes the objects through eqObjects first to check if they contain matching properties.
 
 const assertObjectsEqual = function(object1, object2) {  
   const inspect = require('util').inspect; // <= add this line to get access to the inspect library
@@ -16,6 +15,5 @@ const assertObjectsEqual = function(object1, object2) {
     console.log(`💥 Object assertion failed: ${inspect(object1)} is different from ${inspect(object2)}`);
   }
 };
-
 
 module.exports = assertObjectsEqual;  //exports our assertObjectsEqual function to communicate across modules

c8d036df9e709f2e5ee747bb78c0df34facea9db
ChrisPytel <chris_pytel@hotmail.com>
2024-02-16 20:48:57 -0500

    index.js created - added all of our lotide functions to module.exports object

---
+++ index.js
@@ -0,0 +1,43 @@
+//"index.js created - added all of our lotide functions to module.exports object"
+
+//functions that return console.logs
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+//functions that return a value, object or boolean
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./takeUntil');
+
+
+module.exports = {
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  tail: tail,
+  middle: middle,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
